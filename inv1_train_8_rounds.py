a441ebb3a60a468c5ad3c8ef279f3f3e2d42dfd2
clchinara <audrey.chinara@gmail.com>
2023-09-07 15:08:50 +0800

    Change depth

--- inv1_train_8_rounds.py
+++ inv1_train_8_rounds.py
@@ -1,3 +1,3 @@
 import inv1_train_nets as inv1_tn
 
-inv1_tn.train_speck_distinguisher(200,num_rounds=8,depth=10, num_train_data=10**7);
+inv1_tn.train_speck_distinguisher(200,num_rounds=8,depth=1, num_train_data=10**7);
