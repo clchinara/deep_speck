5edc760945ae7eb6e04647a7f20d42361f075305
Vihaan Philip <viha0001@student.monash.edu>
2023-09-14 12:30:29 +0800

    change depth

--- inv2_train_8_rounds.py
+++ inv2_train_8_rounds.py
@@ -2,4 +2,4 @@ import inv2_train_nets as inv2_tn
 import constants as cs
 
 for diff in cs.DIFFS:
-    inv2_tn.train_speck_distinguisher(200,num_rounds=8,depth=10, diff=diff, subdir=f'{diff}/');
+    inv2_tn.train_speck_distinguisher(200,num_rounds=8,depth=1, diff=diff, subdir=f'{diff}/');
