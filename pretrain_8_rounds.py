f434ec0d5f99d5960046faab6f26bb1c7681b487
Student <audrey.chinara@gmail.com>
2023-09-28 03:12:38 +0800

    Update pretraining for 8 rounds

--- pretrain_8_rounds.py
+++ pretrain_8_rounds.py
@@ -11,6 +11,6 @@ diffB = (32832, 33088)
 model_dir = f'{INV_NAME}_freshly_trained_nets'
 
 net7 = load_model(f'{model_dir}/best7depth1.h5')
-tn.pretrain_8_rounds(net7=net7, diffA=diffA, diffB=diffB)
+tn.pretrain_8_rounds(net7=net7, diffA=diffA, diffB=diffB, num_epochs=30)
 
 
