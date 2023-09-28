bc6e1f955d8c1e3f40cdb728d9fa6d43e5bb6d82
clchinara <audrey.chinara@gmail.com>
2023-09-28 21:05:55 +0800

    Add script to train 8 rounds

---
+++ train_8_rounds.py
@@ -0,0 +1,11 @@
+import speck as sp
+import train_nets as tn
+
+from keras.models import load_model
+
+from constants import INV_NAME
+
+model_dir = f'{INV_NAME}_freshly_trained_nets'
+pretrained_net = load_model(f'{model_dir}/bestpretrain8.h5')
+
+tn.train_speck_distinguisher_8_rounds(pretrained_net=pretrained_net)
