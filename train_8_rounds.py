92fc0f0ae10d3c2ec4b68ea942a9a753a6752075
clchinara <audrey.chinara@gmail.com>
2023-12-12 22:10:05 +0700

    Increase training epochs

--- train_8_rounds.py
+++ train_8_rounds.py
@@ -8,4 +8,4 @@ from constants import INV_NAME
 model_dir = f'{INV_NAME}_freshly_trained_nets'
 pretrained_net = load_model(f'{model_dir}/bestpretrain8i0_12_12.h5')
 
-tn.train_speck_distinguisher_8_rounds(pretrained_net=pretrained_net, file_name_suffix='12_12', num_epochs=20)
+tn.train_speck_distinguisher_8_rounds(pretrained_net=pretrained_net, file_name_suffix='12_12', num_epochs=40)
